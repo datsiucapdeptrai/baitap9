41dca1763552dc52cedcdd7338db43f97f368630
datsiucapdeptrai <nguyendat2502www@gmail.com>
2026-09-21 15:37:12 +0700

    Delete src directory

--- src/main/java/vn/iostart/productmanagement/repository/ProductImageRepository.java
+++
@@ -1,9 +0,0 @@
-package vn.iostart.productmanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import vn.iostart.productmanagement.entity.ProductImage;
-
-public interface ProductImageRepository
-        extends JpaRepository<ProductImage, Long> {
-}
